829d3b532d1dbe6c4db960dc336b43aece8a605a
dragana <stefanoviic.dragana@gmail.com>
2023-01-23 13:53:36 +0100

    Amin

--- src/components/gallery/PhotosPage.jsx
+++ src/components/gallery/PhotosPage.jsx
@@ -43,7 +43,7 @@ checkPage();
   </ul>
   
     <div className="container">
-<Fancybox props={null}>
+<Fancybox options={null} delegate={"[data-fancybox]"}>
 {
 photos.slice((this_page-1)*12, (this_page-1)*12+12).map(img=>(
         <div className="item"><a  data-fancybox="gallery" href={img.name} ><img src={img.name} alt={alt} className="gallery-image"></img></a></div>
