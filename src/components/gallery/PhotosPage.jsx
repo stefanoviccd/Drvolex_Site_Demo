7c5e50cf3eb21f4831e1d6191b0030a3d20803a8
dragana <stefanoviic.dragana@gmail.com>
2023-01-24 10:02:00 +0100

    Pagination again

--- src/components/gallery/PhotosPage.jsx
+++ src/components/gallery/PhotosPage.jsx
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import Divider from '../divider/Divider';
-import Pagination from '../Pagination';
 import Fancybox from '../Fancybox';
 import "./photosPage.css";
 
